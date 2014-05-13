9ec44de298464344aa50bb9ab2aa84f3f0bd925c
kbbryant <kbbryant@ucsc.edu>
2014-05-13 12:41:20 -0700

    Check for new rounds and crashes

---
+++ Assets/Scripts/MouseOrbit.cs
@@ -0,0 +1,68 @@
+﻿// Converted from UnityScript to C# at http://www.M2H.nl/files/js_to_c.php - by Mike Hergaarden
+// Do test the code! You usually need to change a few small bits.
+
+using UnityEngine;
+using System.Collections;
+
+public class MouseOrbit : MonoBehaviour
+{
+    public static GameObject target;
+    float distanceMin = 10.0f;
+    float distanceMax = 15.0f;
+    float distanceInitial = 12.5f;
+    float scrollSpeed = 1.0f;
+
+    float xSpeed = 250.0f;
+    float ySpeed = 120.0f;
+
+    int yMinLimit = -20;
+    int yMaxLimit = 80;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+    private float distanceCurrent;
+
+    [AddComponentMenu("Camera-Control/Mouse Orbit")]
+    //partial class MouseOrbit { }
+
+    void Start()
+    {
+        distanceCurrent = distanceInitial;
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        // Make the rigid body not change rotation
+        if (rigidbody)
+            rigidbody.freezeRotation = true;
+    }
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+            distanceCurrent -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+
+            distanceCurrent = Mathf.Clamp(distanceCurrent, distanceMin, distanceMax);
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            Vector3 multiplier = new Vector3(0.0f, 0.0f, -distanceCurrent);
+            Vector3 position = rotation * multiplier + target.transform.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Scripts/roundResults.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roundResults : MonoBehaviour
+{
+
+    public static bool crashed;
+    public static int roundNum;
+    public int numOfRounds;
+    public bool landed;
+    public Vector3 spawnPos;
+    public Vector3 endPos;
+    public static GameObject[] spaceshipArray;
+    public GameObject payload;
+
+    //private MouseOrbit cam;
+    // Use this for initialization
+    void Start()
+    {
+        numOfRounds = 4;
+        spaceshipArray = new GameObject[numOfRounds];
+        // spaceshipArray[0] = GameObject.Find("ShippingContainer");
+         spawnPos = payload.transform.position;
+        //MouseOrbit.target  = spaceshipArray[0];
+         roundNum = 0;
+         landed = false;
+         crashed = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //newRounds();
+    }
+
+       void OnCollisionEnter(Collision collision)
+          {
+              if (collision.gameObject.tag == "Terrain" || collision.gameObject.tag == "MarsTerrain")
+              {
+
+                  if (InGameUI.velNum < 10.0)
+                  {
+                     landed = true;
+                  }
+                  else
+                  {
+                      crashed = true;
+                  }
+              }
+        }       
+
+    void OnGUI()
+    {
+        if (roundNum < numOfRounds)
+        {
+            if (landed == true)
+            {
+                print("landed");
+                if (GUI.Button(new Rect(Screen.width * 0.5f - 40, Screen.height * 0.5f - 20, 80, 40), "Next Launch"))
+                {
+
+                    endPos = payload.transform.position;
+                    spaceshipArray[roundNum] = (GameObject)Instantiate(Resources.Load("SpaceshipDummy"), endPos, transform.rotation);
+                    MouseOrbit.target = payload;
+                    payload.transform.position = spawnPos;
+                    landed = false;
+                    crashed = false;
+                    roundNum++;
+                }
+            }
+            else if (crashed == true)
+            {
+                Debug.Log("CRASSSSSSSSHED!!!!");
+                if (GUI.Button(new Rect(Screen.width * 0.5f - 40, Screen.height * 0.5f - 20, 80, 40), "Continue?"))
+                {
+                    endPos = payload.transform.position;
+                    spaceshipArray[roundNum] = (GameObject)Instantiate(Resources.Load("SpaceshipDummy"), endPos, transform.rotation);
+                    MouseOrbit.target = payload;
+                    payload.transform.position = spawnPos;
+                    landed = false;
+                    crashed = false;
+                    roundNum++;
+                }
+            }
+            
+        }
+
+    }
+
+}
