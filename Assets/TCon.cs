014a4f9f54e0fb67c6f66272ab7a58c55b7a1fff
Tsampurado <mancheta@ucsc.edu>
2014-05-15 08:14:55 -0700

    bgm is in

--- Assets/TCon.cs
+++ Assets/TCon.cs
@@ -54,7 +54,7 @@ public class TCon : MonoBehaviour {
 			doorRightT.Play ();
 			endLeftT.Play ();
 			endRightT.Play ();
-			rigidbody.AddForce(locusC.transform.up * (thrust * 10));
+			rigidbody.AddForce(locusC.transform.up * (thrust));
 		}
 		/*else {
 		//else if((Input.GetAxis("Vertical") == 0) || (Input.GetAxis ("Horizontal") == 0)) { //checking if no Vert keys are down
