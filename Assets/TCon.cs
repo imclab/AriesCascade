38f65ae1941faba4068074965851c7e04b6e5078
DamagedPandaGame <cocoonedgame@gmail.com>
2014-05-15 09:35:04 -0700

    fixed rounds

--- Assets/TCon.cs
+++ Assets/TCon.cs
@@ -56,7 +56,7 @@ public class TCon : MonoBehaviour {
 			endLeftT.Play ();
 			endRightT.Play ();
 			rigidbody.AddForce(locusC.transform.up * (thrust * 10));
-            sound.GetComponent<GameSounds>().playThrust();
+            //sound.GetComponent<GameSounds>().playThrust();
 		}
 		/*else {
 		//else if((Input.GetAxis("Vertical") == 0) || (Input.GetAxis ("Horizontal") == 0)) { //checking if no Vert keys are down
